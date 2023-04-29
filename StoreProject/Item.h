1d34b38c7fd8334689c2b2983c81ec0f3baf5a11
M4NIS-H <82280209+M4NIS-H@users.noreply.github.com>
2023-04-29 17:27:00 -0500

    Add files via upload

---
+++ StoreProject/Item.h
@@ -0,0 +1,34 @@
+#ifndef ITEM_H
+#define ITEM_H
+
+#include <string>
+
+class Item {
+public:
+    // Constructors
+    Item(std::string name, std::string category, double price, int stock);
+    Item(std::string name, std::string category, double price, int stock, int amount);
+
+    // Accessors
+    std::string getName() const;
+    std::string getCategory() const;
+    double getPrice() const;
+    int getStock() const;
+    int getAmount() const;
+
+    // Member functions
+    void printInfo() const;
+    bool setStock(std::string name, int amount);
+    static void viewCategory(std::string category);
+
+  
+
+private:
+    std::string name;
+    double price;
+    int stock;
+    std::string category;
+    int amount;
+};
+
+#endif
